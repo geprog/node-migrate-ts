178f39beeb1d983c07d2c8c6fbfce42af925d0b5
Lukas <lukas@slucky.de>
2022-01-19 21:43:46 +0100

    fix: export MigrationModel and MigrationStore inferfaces (#39)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Migration, MigrationContext } from './Migration';
+import { MigrationModel, MigrationStore } from './MigrationStore';
 import { MongoMigrationStore } from './MongoMigrationStore';
 import up from './up';
 
-export { Migration, MigrationContext, MongoMigrationStore, up };
+export { Migration, MigrationContext, MigrationModel, MigrationStore, MongoMigrationStore, up };
